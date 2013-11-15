96035b46585c202a6bb3f876753e0f2d8282d52e
Ignacio Olaciregui <ignacioola@gmail.com>
2013-11-15 12:09:25 -0300

    initial commit

---
+++ build/build.js
@@ -0,0 +1,829 @@
+
+/**
+ * Require the given path.
+ *
+ * @param {String} path
+ * @return {Object} exports
+ * @api public
+ */
+
+function require(path, parent, orig) {
+  var resolved = require.resolve(path);
+
+  // lookup failed
+  if (null == resolved) {
+    orig = orig || path;
+    parent = parent || 'root';
+    var err = new Error('Failed to require "' + orig + '" from "' + parent + '"');
+    err.path = orig;
+    err.parent = parent;
+    err.require = true;
+    throw err;
+  }
+
+  var module = require.modules[resolved];
+
+  // perform real require()
+  // by invoking the module's
+  // registered function
+  if (!module._resolving && !module.exports) {
+    var mod = {};
+    mod.exports = {};
+    mod.client = mod.component = true;
+    module._resolving = true;
+    module.call(this, mod.exports, require.relative(resolved), mod);
+    delete module._resolving;
+    module.exports = mod.exports;
+  }
+
+  return module.exports;
+}
+
+/**
+ * Registered modules.
+ */
+
+require.modules = {};
+
+/**
+ * Registered aliases.
+ */
+
+require.aliases = {};
+
+/**
+ * Resolve `path`.
+ *
+ * Lookup:
+ *
+ *   - PATH/index.js
+ *   - PATH.js
+ *   - PATH
+ *
+ * @param {String} path
+ * @return {String} path or null
+ * @api private
+ */
+
+require.resolve = function(path) {
+  if (path.charAt(0) === '/') path = path.slice(1);
+
+  var paths = [
+    path,
+    path + '.js',
+    path + '.json',
+    path + '/index.js',
+    path + '/index.json'
+  ];
+
+  for (var i = 0; i < paths.length; i++) {
+    var path = paths[i];
+    if (require.modules.hasOwnProperty(path)) return path;
+    if (require.aliases.hasOwnProperty(path)) return require.aliases[path];
+  }
+};
+
+/**
+ * Normalize `path` relative to the current path.
+ *
+ * @param {String} curr
+ * @param {String} path
+ * @return {String}
+ * @api private
+ */
+
+require.normalize = function(curr, path) {
+  var segs = [];
+
+  if ('.' != path.charAt(0)) return path;
+
+  curr = curr.split('/');
+  path = path.split('/');
+
+  for (var i = 0; i < path.length; ++i) {
+    if ('..' == path[i]) {
+      curr.pop();
+    } else if ('.' != path[i] && '' != path[i]) {
+      segs.push(path[i]);
+    }
+  }
+
+  return curr.concat(segs).join('/');
+};
+
+/**
+ * Register module at `path` with callback `definition`.
+ *
+ * @param {String} path
+ * @param {Function} definition
+ * @api private
+ */
+
+require.register = function(path, definition) {
+  require.modules[path] = definition;
+};
+
+/**
+ * Alias a module definition.
+ *
+ * @param {String} from
+ * @param {String} to
+ * @api private
+ */
+
+require.alias = function(from, to) {
+  if (!require.modules.hasOwnProperty(from)) {
+    throw new Error('Failed to alias "' + from + '", it does not exist');
+  }
+  require.aliases[to] = from;
+};
+
+/**
+ * Return a require function relative to the `parent` path.
+ *
+ * @param {String} parent
+ * @return {Function}
+ * @api private
+ */
+
+require.relative = function(parent) {
+  var p = require.normalize(parent, '..');
+
+  /**
+   * lastIndexOf helper.
+   */
+
+  function lastIndexOf(arr, obj) {
+    var i = arr.length;
+    while (i--) {
+      if (arr[i] === obj) return i;
+    }
+    return -1;
+  }
+
+  /**
+   * The relative require() itself.
+   */
+
+  function localRequire(path) {
+    var resolved = localRequire.resolve(path);
+    return require(resolved, parent, path);
+  }
+
+  /**
+   * Resolve relative to the parent.
+   */
+
+  localRequire.resolve = function(path) {
+    var c = path.charAt(0);
+    if ('/' == c) return path.slice(1);
+    if ('.' == c) return require.normalize(p, path);
+
+    // resolve deps by returning
+    // the dep in the nearest "deps"
+    // directory
+    var segs = parent.split('/');
+    var i = lastIndexOf(segs, 'deps') + 1;
+    if (!i) i = 0;
+    path = segs.slice(0, i + 1).join('/') + '/deps/' + path;
+    return path;
+  };
+
+  /**
+   * Check if module is defined at `path`.
+   */
+
+  localRequire.exists = function(path) {
+    return require.modules.hasOwnProperty(localRequire.resolve(path));
+  };
+
+  return localRequire;
+};
+require.register("component-indexof/index.js", function(exports, require, module){
+module.exports = function(arr, obj){
+  if (arr.indexOf) return arr.indexOf(obj);
+  for (var i = 0; i < arr.length; ++i) {
+    if (arr[i] === obj) return i;
+  }
+  return -1;
+};
+});
+require.register("component-emitter/index.js", function(exports, require, module){
+
+/**
+ * Module dependencies.
+ */
+
+var index = require('indexof');
+
+/**
+ * Expose `Emitter`.
+ */
+
+module.exports = Emitter;
+
+/**
+ * Initialize a new `Emitter`.
+ *
+ * @api public
+ */
+
+function Emitter(obj) {
+  if (obj) return mixin(obj);
+};
+
+/**
+ * Mixin the emitter properties.
+ *
+ * @param {Object} obj
+ * @return {Object}
+ * @api private
+ */
+
+function mixin(obj) {
+  for (var key in Emitter.prototype) {
+    obj[key] = Emitter.prototype[key];
+  }
+  return obj;
+}
+
+/**
+ * Listen on the given `event` with `fn`.
+ *
+ * @param {String} event
+ * @param {Function} fn
+ * @return {Emitter}
+ * @api public
+ */
+
+Emitter.prototype.on =
+Emitter.prototype.addEventListener = function(event, fn){
+  this._callbacks = this._callbacks || {};
+  (this._callbacks[event] = this._callbacks[event] || [])
+    .push(fn);
+  return this;
+};
+
+/**
+ * Adds an `event` listener that will be invoked a single
+ * time then automatically removed.
+ *
+ * @param {String} event
+ * @param {Function} fn
+ * @return {Emitter}
+ * @api public
+ */
+
+Emitter.prototype.once = function(event, fn){
+  var self = this;
+  this._callbacks = this._callbacks || {};
+
+  function on() {
+    self.off(event, on);
+    fn.apply(this, arguments);
+  }
+
+  fn._off = on;
+  this.on(event, on);
+  return this;
+};
+
+/**
+ * Remove the given callback for `event` or all
+ * registered callbacks.
+ *
+ * @param {String} event
+ * @param {Function} fn
+ * @return {Emitter}
+ * @api public
+ */
+
+Emitter.prototype.off =
+Emitter.prototype.removeListener =
+Emitter.prototype.removeAllListeners =
+Emitter.prototype.removeEventListener = function(event, fn){
+  this._callbacks = this._callbacks || {};
+
+  // all
+  if (0 == arguments.length) {
+    this._callbacks = {};
+    return this;
+  }
+
+  // specific event
+  var callbacks = this._callbacks[event];
+  if (!callbacks) return this;
+
+  // remove all handlers
+  if (1 == arguments.length) {
+    delete this._callbacks[event];
+    return this;
+  }
+
+  // remove specific handler
+  var i = index(callbacks, fn._off || fn);
+  if (~i) callbacks.splice(i, 1);
+  return this;
+};
+
+/**
+ * Emit `event` with the given args.
+ *
+ * @param {String} event
+ * @param {Mixed} ...
+ * @return {Emitter}
+ */
+
+Emitter.prototype.emit = function(event){
+  this._callbacks = this._callbacks || {};
+  var args = [].slice.call(arguments, 1)
+    , callbacks = this._callbacks[event];
+
+  if (callbacks) {
+    callbacks = callbacks.slice(0);
+    for (var i = 0, len = callbacks.length; i < len; ++i) {
+      callbacks[i].apply(this, args);
+    }
+  }
+
+  return this;
+};
+
+/**
+ * Return array of callbacks for `event`.
+ *
+ * @param {String} event
+ * @return {Array}
+ * @api public
+ */
+
+Emitter.prototype.listeners = function(event){
+  this._callbacks = this._callbacks || {};
+  return this._callbacks[event] || [];
+};
+
+/**
+ * Check if this emitter has `event` handlers.
+ *
+ * @param {String} event
+ * @return {Boolean}
+ * @api public
+ */
+
+Emitter.prototype.hasListeners = function(event){
+  return !! this.listeners(event).length;
+};
+
+});
+require.register("component-query/index.js", function(exports, require, module){
+function one(selector, el) {
+  return el.querySelector(selector);
+}
+
+exports = module.exports = function(selector, el){
+  el = el || document;
+  return one(selector, el);
+};
+
+exports.all = function(selector, el){
+  el = el || document;
+  return el.querySelectorAll(selector);
+};
+
+exports.engine = function(obj){
+  if (!obj.one) throw new Error('.one callback required');
+  if (!obj.all) throw new Error('.all callback required');
+  one = obj.one;
+  exports.all = obj.all;
+  return exports;
+};
+
+});
+require.register("component-matches-selector/index.js", function(exports, require, module){
+/**
+ * Module dependencies.
+ */
+
+var query = require('query');
+
+/**
+ * Element prototype.
+ */
+
+var proto = Element.prototype;
+
+/**
+ * Vendor function.
+ */
+
+var vendor = proto.matches
+  || proto.webkitMatchesSelector
+  || proto.mozMatchesSelector
+  || proto.msMatchesSelector
+  || proto.oMatchesSelector;
+
+/**
+ * Expose `match()`.
+ */
+
+module.exports = match;
+
+/**
+ * Match `el` to `selector`.
+ *
+ * @param {Element} el
+ * @param {String} selector
+ * @return {Boolean}
+ * @api public
+ */
+
+function match(el, selector) {
+  if (vendor) return vendor.call(el, selector);
+  var nodes = query.all(selector, el.parentNode);
+  for (var i = 0; i < nodes.length; ++i) {
+    if (nodes[i] == el) return true;
+  }
+  return false;
+}
+
+});
+require.register("discore-closest/index.js", function(exports, require, module){
+var matches = require('matches-selector')
+
+module.exports = function (element, selector, checkYoSelf, root) {
+  element = checkYoSelf ? element : element.parentNode
+  root = root || document
+
+  do {
+    if (matches(element, selector))
+      return element
+    // After `matches` on the edge case that
+    // the selector matches the root
+    // (when the root is not the document)
+    if (element === root)
+      return
+    // Make sure `element !== document`
+    // otherwise we get an illegal invocation
+  } while ((element = element.parentNode) && element !== document)
+}
+});
+require.register("component-event/index.js", function(exports, require, module){
+var bind = (window.addEventListener !== undefined) ? 'addEventListener' : 'attachEvent',
+    unbind = (window.removeEventListener !== undefined) ? 'removeEventListener' : 'detachEvent',
+    prefix = (bind !== 'addEventListener') ? 'on' : '';
+
+/**
+ * Bind `el` event `type` to `fn`.
+ *
+ * @param {Element} el
+ * @param {String} type
+ * @param {Function} fn
+ * @param {Boolean} capture
+ * @return {Function}
+ * @api public
+ */
+
+exports.bind = function(el, type, fn, capture){
+  el[bind](prefix + type, fn, capture || false);
+
+  return fn;
+};
+
+/**
+ * Unbind `el` event `type`'s callback `fn`.
+ *
+ * @param {Element} el
+ * @param {String} type
+ * @param {Function} fn
+ * @param {Boolean} capture
+ * @return {Function}
+ * @api public
+ */
+
+exports.unbind = function(el, type, fn, capture){
+  el[unbind](prefix + type, fn, capture || false);
+
+  return fn;
+};
+});
+require.register("component-delegate/index.js", function(exports, require, module){
+/**
+ * Module dependencies.
+ */
+
+var closest = require('closest')
+  , event = require('event');
+
+/**
+ * Delegate event `type` to `selector`
+ * and invoke `fn(e)`. A callback function
+ * is returned which may be passed to `.unbind()`.
+ *
+ * @param {Element} el
+ * @param {String} selector
+ * @param {String} type
+ * @param {Function} fn
+ * @param {Boolean} capture
+ * @return {Function}
+ * @api public
+ */
+
+exports.bind = function(el, selector, type, fn, capture){
+  return event.bind(el, type, function(e){
+    var target = e.target || e.srcElement;
+    e.delegateTarget = closest(target, selector, true, el);
+    if (e.delegateTarget) fn.call(el, e);
+  }, capture);
+};
+
+/**
+ * Unbind event `type`'s callback `fn`.
+ *
+ * @param {Element} el
+ * @param {String} type
+ * @param {Function} fn
+ * @param {Boolean} capture
+ * @api public
+ */
+
+exports.unbind = function(el, type, fn, capture){
+  event.unbind(el, type, fn, capture);
+};
+
+});
+require.register("component-bind/index.js", function(exports, require, module){
+
+/**
+ * Slice reference.
+ */
+
+var slice = [].slice;
+
+/**
+ * Bind `obj` to `fn`.
+ *
+ * @param {Object} obj
+ * @param {Function|String} fn or string
+ * @return {Function}
+ * @api public
+ */
+
+module.exports = function(obj, fn){
+  if ('string' == typeof fn) fn = obj[fn];
+  if ('function' != typeof fn) throw new Error('bind() requires a function');
+  var args = [].slice.call(arguments, 2);
+  return function(){
+    return fn.apply(obj, args.concat(slice.call(arguments)));
+  }
+};
+
+});
+require.register("pie/index.js", function(exports, require, module){
+/**
+ * Adapted from this example: 
+ *      - http://jbkflex.wordpress.com/2011/07/28/creating-a-svg-pie-chart-html5/
+ */
+
+/**
+ * Module dependencies.
+ */
+
+var Emitter = require('emitter');
+var delegate = require('delegate');
+var bind = require('bind');
+
+/**
+ * SVG namespace.
+ */
+
+var svgns = "http://www.w3.org/2000/svg";
+
+/**
+ * Default values.
+ */
+
+var width = 70;
+var height = 70;
+var colors = ["#79b2e3", "#e57ad1", "#3F7C20", "#F06078", "#F0C000", "#903060", 
+  "#F09000", "#55BF3B", "#DF5353", "#7798BF", "#D8DA86"];
+
+/**
+ * Export `Pie`
+ */
+
+module.exports = Pie
+
+/**
+ * `Pie` constructor.
+ *
+ * @argument {Array} data The list of data.
+ *
+ *    Accepted formats:
+ *      
+ *        - [1, 2, 3]
+ *        - [[1, 'label1'], [2, 'label2']]
+ *        - [[1, 'label1' , 'red'], [2, 'label2', 'blue']]
+ */
+
+function Pie(data) {
+
+  if (!(this instanceof Pie)) {
+    return new Pie(data);
+  }
+
+  this.binds = {
+    onmouseover: bind(this, this.onmouseover)
+  };
+
+  this.data(data);
+}
+
+/**
+ * Load data for the pie and compute total and angles.
+ *
+ * @api private
+ */
+
+Pie.prototype.data = function(data) {
+  var val;
+
+  this.datums = data;
+  this.values = [];
+  this.colors = [];
+
+  // add up the data values so we know how big the pie is
+  this.total = 0;
+  for(var i = 0; i < data.length; i++) {
+    val = data[i];
+
+    // storing color for easier access.
+    this.colors.push(val[2]);
+
+    if (!(typeof val === "number")) {
+      val = val[0];
+    }
+    
+    this.values.push(val);
+    this.total += val;
+  }
+
+  // figure out how big each slice of pie is. Angles in radians.
+  this.angles = [];
+  for(var i = 0; i < data.length; i++) {
+    this.angles[i] = this.values[i] / this.total * Math.PI * 2;
+  }
+}
+
+/**
+ * Set the dimensions of the pie chart
+ *
+ * @api public
+ */
+
+Pie.prototype.size = function(width, height) {
+  this.width = width;
+  this.height = height;
+
+  return this;
+}
+
+/**
+ * Renders the pie chart
+ *
+ * @api public
+ */
+
+Pie.prototype.render = function() {
+  var values = this.values;
+  var angles = this.angles;
+
+  var w = this.width || width;    // The width of the chart
+  var h = this.height || height;  // The height of the chart
+  var cx = w/2;                   // The center on x
+  var cy = h/2;                   // The cenger on y
+  var r = cy;                     // The radius
+
+  var el = document.createElementNS(svgns, "svg:svg");
+
+  el.setAttribute("width", w);
+  el.setAttribute("height", h);
+  el.setAttribute("viewBox", "0 0 " + w + " " + h);
+
+  // Loop through each slice of pie.
+  var startangle = 0;
+  for(var i = 0; i < values.length; i++) {
+
+    // This is where the wedge ends
+    var endangle = startangle + angles[i];
+
+    // Compute the two points where our wedge intersects the circle
+    // These formulas are chosen so that an angle of 0 is at 12 o'clock
+    // and positive angles increase clockwise.
+    var x1 = cx + r * Math.sin(startangle);
+    var y1 = cy - r * Math.cos(startangle);
+    var x2 = cx + r * Math.sin(endangle);
+    var y2 = cy - r * Math.cos(endangle);
+
+    // This is a flag for angles larger than than a half circle
+    // It is required by the SVG arc drawing component
+    var big = 0;
+    if (endangle - startangle > Math.PI) big = 1;
+
+    // We describe a wedge with an <svg:path> element
+    var path = document.createElementNS(svgns, "path");
+
+    // This string holds the path details
+    var d = "M " + cx + "," + cy + // Start at circle center
+      " L " + x1 + "," + y1 +      // Draw line to (x1,y1)
+      " A " + r + "," + r +        // Draw an arc of radius r
+      " 0 " + big + " 1 " +        // Arc details...
+      x2 + "," + y2 +              // Arc goes to to (x2,y2)
+      " Z";                        // Close path back to (cx,cy)
+
+    // Now set attributes on the <svg:path> element
+    path.setAttribute("d", d);
+    path.setAttribute("fill", this.colors[i] 
+      || colors[i % colors.length]);
+    path.setAttribute("stroke", "#FFF");
+    path.setAttribute("stroke-width", "1");
+    
+    // Store index on the path
+    path.__idx =  i;
+
+    // Add wedge to chart
+    el.appendChild(path);                   
+
+    // The next wedge begins where this one ends
+    startangle = endangle;
+  }
+  
+  // Unbind events if it was previously rendered.
+  if (this.el) {
+    delegate.unbind(el, 'mouseover', 
+      this.bind.onmouseover);
+  }
+
+  // Bind events
+  delegate.bind(el, 'path', 'mouseover', 
+    this.binds.onmouseover);
+
+  this.el = el;
+
+  return el;
+}
+
+/**
+ * Mouseover handler
+ *
+ * @api private
+ */
+
+Pie.prototype.onmouseover = function(e) {
+  var i = e.target.__idx;
+  var d = this.datums[i];
+  var val = this.values[i];
+  var col = this.colors[i] || 
+    colors[i % colors.length];
+
+  this.emit('hover', i, {
+      value: val, 
+      label: d[1], 
+      color: col,
+      share: val * 100 / this.total
+    });
+}
+
+/**
+ * Emitter mixin.
+ */
+
+Emitter(Pie.prototype);
+
+
+});
+
+
+
+
+
+
+
+
+require.alias("component-emitter/index.js", "pie/deps/emitter/index.js");
+require.alias("component-emitter/index.js", "emitter/index.js");
+require.alias("component-indexof/index.js", "component-emitter/deps/indexof/index.js");
+
+require.alias("component-delegate/index.js", "pie/deps/delegate/index.js");
+require.alias("component-delegate/index.js", "delegate/index.js");
+require.alias("discore-closest/index.js", "component-delegate/deps/closest/index.js");
+require.alias("discore-closest/index.js", "component-delegate/deps/closest/index.js");
+require.alias("component-matches-selector/index.js", "discore-closest/deps/matches-selector/index.js");
+require.alias("component-query/index.js", "component-matches-selector/deps/query/index.js");
+
+require.alias("discore-closest/index.js", "discore-closest/index.js");
+require.alias("component-event/index.js", "component-delegate/deps/event/index.js");
+
+require.alias("component-bind/index.js", "pie/deps/bind/index.js");
+require.alias("component-bind/index.js", "bind/index.js");
+
+require.alias("pie/index.js", "pie/index.js");
